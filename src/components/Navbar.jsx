f81e2f0259da9e7ebc77fea6e156801d1e9c47d8
vito <familiacarvalhofeliz@gmail.com>
2025-11-14 20:57:58 -0300

    add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,24 +5,31 @@ import Button from "./Button.jsx";
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <header
-      className="flex items-center justify-between h-24 w-full px-12"
-    >
+    <header className="flex items-center justify-between h-24 w-full px-12">
       {!isMenuOpen && (
-        <h1 className="text-white text-2xl font-semibold">Wallet</h1>
+        <a
+          href="#"
+          className="text-white text-2xl font-semibold hover:text-gray-300"
+        >
+          Wallet
+        </a>
       )}
       <div className={`flex gap-7 ${!isMenuOpen && "max-sm:hidden"}`}>
-        <button className="text-white text-lg">Sign up</button>
-        <Button bgClass="bg-[#BFAFF2]">Log in</Button>
+        <button className="text-white hover:text-gray-300 text-lg">
+          Sign up
+        </button>
+        <Button bgClass="bg-[#BFAFF2]" bgHoverClass="hover:bg-[#A594D6]">
+          Log in
+        </Button>
       </div>
       <button
         className={!isMenuOpen && "sm:hidden"}
         onClick={() => setIsMenuOpen(!isMenuOpen)}
       >
         {isMenuOpen ? (
-          <X size={30} className="text-white hover:text-gray-300 mt-1" />
+          <X size={30} className="text-white hover:text-gray-300" />
         ) : (
-          <Menu size={30} className="text-white hover:text-gray-300 mt-1" />
+          <Menu size={30} className="text-white hover:text-gray-300" />
         )}
       </button>
     </header>
