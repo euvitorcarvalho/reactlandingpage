ebbca0bafa5b3f49e6c443146032bab43aa10e08
vito <familiacarvalhofeliz@gmail.com>
2025-11-15 12:30:50 -0300

    add hover transition

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 function MainSection() {
   return (
-    <div className="text-white flex flex-col gap-10 items-center bg-[url('/src/assets/background.png')] bg-no-repeat bg-cover bg-bottom lg:h-[800px] h-[867px] w-full pt-16 px-5 lg:pt-20">
+    <div className="text-white flex flex-col gap-10 items-center bg-[url('/src/assets/background.png')] bg-no-repeat bg-cover bg-bottom h-[867px] w-full pt-16 px-5 lg:pt-20">
       <h1 className="text-6xl text-center font-semibold max-sm:text-4xl">
         Saas Landing Page Template
       </h1>
@@ -10,7 +10,7 @@ function MainSection() {
         This is a template Figma file, turned into code using React. Learn more
         at AnimaApp.com
       </p>
-      <Button bgClass={"bg-[#F8D57E]"}>Get Started</Button>
+      <Button bgClass={"bg-[#F8D57E]"} bgHoverClass={"hover:bg-[#ffce51]"}>Get Started</Button>
       <div></div>
     </div>
   );
